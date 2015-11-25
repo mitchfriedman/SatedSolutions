729a6fe1d37d6a949d934f249a55bc23af90c90c
mitchfriedman <mitchfriedman5@gmail.com>
2015-11-25 17:12:58 -0500

    Add debug argument to run script

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import sys
 from app.application import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=5000)
+    debug = True if len(sys.argv) > 1 else False
+    app.run(host='0.0.0.0', debug=debug, port=5000)
