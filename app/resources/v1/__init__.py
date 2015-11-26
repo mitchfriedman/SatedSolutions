93316dcf19d9d5a6e9a624981fd6c61d6946a49a
Alex <alex@ubuntu.(none)>
2015-11-25 23:49:08 -0500

    added endpoint for invitation

--- app/resources/v1/__init__.py
+++ app/resources/v1/__init__.py
@@ -18,6 +18,7 @@ api.add_resource(Logout, '/api/Logout')
 
 api.add_resource(Teams, '/api/Teams')
 api.add_resource(Team, '/api/Teams/<string:team_unid>')
+api.add_resource(Invitation, '/api/Teams/<string:team_unid>/Invitation')
 
 api.add_resource(Participants, '/api/Teams/<string:team_unid>/Participants')
 
