8f6cdc1f14ef6d03216b84a73c9edbffb03782fd
mitch <mitchfriedman5@gmail.com>
2015-11-26 15:07:08 +0000

    Add threading

--- run.py
+++ run.py
@@ -16,5 +16,5 @@ def after_request(response):
 
 if __name__ == '__main__':
     debug = True if len(sys.argv) > 1 else False
-    app.run(host='0.0.0.0', debug=debug, port=5000)
+    app.run(host='0.0.0.0', debug=debug, port=5000, threaded=True)
 
